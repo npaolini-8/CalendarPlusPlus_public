c7977658d4ecae22d4c0a4d9bd7396d768a53055
BlackSwan-Decoded <56172407+BlackSwan-Decoded@users.noreply.github.com>
2021-10-18 11:52:54 -0400

    modified templates to use inheritance

--- flaskr/app.py
+++ flaskr/app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
    return render_template('index.html')
 
+@app.route('/calendar')
+def myCalendar():
+    return render_template('calendar.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)

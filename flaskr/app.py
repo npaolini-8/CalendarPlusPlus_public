c06f4a664a4a66f5f07852dd0ccae196d815eed2
JehuA <jehujananoria@gmail.com>
2021-10-25 12:01:04 -0400

    Changed the base.html

--- flaskr/app.py
+++ flaskr/app.py
@@ -2,9 +2,11 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/calendar')
 def myCalendar():

3ed623e88c19d975e2240d8b13002455113de077
JehuA <jehujananoria@gmail.com>
2021-10-30 13:11:26 -0400

    rebase

---
+++ flaskr/views/__init__.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template, url_for
+
+main_blueprint = Blueprint("main", __name__, static_folder="../static")
+
+
+@main_blueprint.route('/')
+def home():
+    return render_template('index.html')
+
+
+def init_app(app):
+    # index route
+    app.register_blueprint(main_blueprint)
+
+    from .calendar import cal_blueprint as calendar
+    app.register_blueprint(calendar)
---
+++ flaskr/views/calendar.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, render_template
+
+
+cal_blueprint = Blueprint("calendar", __name__, url_prefix='/calendar')
+
+
+@cal_blueprint.route('/my_calendar')
+def calendar():
+    return render_template('calendar/calendar.html')
+

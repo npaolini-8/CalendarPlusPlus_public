282c088eb0b485564358159f7c5347d0951083c6
Nick Paolini <npaolini@gmail.com>
2021-11-01 00:55:06 -0400

    csv writing added, some comments

--- mongo.py
+++ mongo.py
@@ -1,8 +1,11 @@
 from os import write
 from pymongo import MongoClient # MongoDB Library to connect to database.
-from datetime import datetime # Used to get timestamps for database information
+from datetime import datetime, tzinfo # Used to get timestamps for database information
 import ssl # Used to specify certificate connection for MongoDB
 from icalendar import Calendar, Event
+import pytz
+from typing import List
+import csv
 
 
 cluster = MongoClient("mongodb+srv://nickp:UEuYChybyfDeiRRq@cal0.uud0f.mongodb.net/test", ssl_cert_reqs=ssl.CERT_NONE)
@@ -52,22 +55,34 @@ def add_friend( username, f_username):
         }
     )
 
+#builds calendar file based on given format: ics, csv
 def export_calendar( username, format):
+
+    #db queries
+    #TODO: Error handling
     user = users_collection.find_one({"username" : username})
     events = user["events"]
     
     if format.lower() == "ics":
+        #generate icalendar object from db events
+        #TODO: do we want to pass username and build that into ical file description?
         cal = build_ics( events )
+        #generates string from ical object
         data = cal.to_ical()
 
+        #writes file
+        #TODO: will we ultimately switch this to string return and pass back to front end?
         with open( "export_test/test.ics", 'wb') as ical_file:
             ical_file.write(data)
     
-    else:
-        build_csv( events )
+    elif format.lower() == "csv" :
+        csv_cal = build_csv( events )
 
-    # for event in events:
-    #     print( event["event_id"] )
+        with open( "export_test/test.csv", 'w', encoding='utf-8') as csv_handler:
+            writer = csv.writer(csv_handler)
+            
+            for row in csv_cal:
+                writer.writerow([r.strip('[]')] for r in row)
         
 
 def build_ics( events ) -> Calendar:
@@ -75,6 +90,11 @@ def build_ics( events ) -> Calendar:
     cal = Calendar()
     id = 0
 
+    #required header for compatibility
+    cal.add('version', '2.0')
+    cal.add('prodid', '-//calendarplusplus export//') #can put anything here wrapped in the slashes i think...
+
+    #icals are populated by event subcomponents, loop builds event subcomponents from DB and adds them to calendar
     for item in events:
         event = Event()
 
@@ -92,7 +112,10 @@ def build_ics( events ) -> Calendar:
 
     return cal
 
-def get_datetime( datestr ):
+#returns datetime object built from datetime string from db
+#format: YYYYMMDDTHHMMSS
+#assumes we are storing in utc, tags objects accordingly
+def get_datetime( datestr ) -> datetime:
 
     year = int(datestr[0:4])
     month = int(datestr[4:6])
@@ -101,14 +124,26 @@ def get_datetime( datestr ):
     hour = int(datestr[9:11])
     minute = int(datestr[11:13])
 
-    return datetime(year,month,day,hour,minute)
+    return datetime(year,month,day,hour,minute, tzinfo=pytz.utc)
 
 
 def build_csv( events):
-    return None
+
+    csv_data : List[List[str]] = []
+
+    #add header
+    #TODO: format?
+    row = ["Description", "Start", "End","Location","Recurrence"]
+    csv_data.append(row)
+
+    for item in events:
+        row = [ item["description"], item["start_time"], item["end_time"], item["location"], str(item["recurrence"])]
+        csv_data.append(row)
+
+    return csv_data
 
 
-export_calendar( "testy", "ics" )
+export_calendar( "testy", "csv" )
 
 #confirmed working
 #create_user("testery","plaintext smile", "Two", "Tester")

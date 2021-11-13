6a7a8253c82e500ca963fd59b857ff4baa3cb4ae
BlackSwan-Decoded <56172407+BlackSwan-Decoded@users.noreply.github.com>
2021-11-13 14:49:48 -0500

    added links to authenticate user

--- flaskr/views/authenticate.py
+++ flaskr/views/authenticate.py
@@ -1,13 +1,49 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, redirect, render_template, request, session, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
 
 login_blueprint = Blueprint("authenticate", __name__, url_prefix='/account')
 
-@login_blueprint.route('/register/', methods=['GET', 'POST'])
+@login_blueprint.route('/register/', methods=('GET', 'POST'))
 def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+
+        if not username:
+            error = 'Username is required'
+        elif not password:
+            error = 'Password is required'
+
+        #if error is None:
+        #flash(error)
     return render_template('account/register.html')
 
 
-@login_blueprint.route('/login/', methods=['GET', 'POST'])
+@login_blueprint.route('/login/', methods=('GET', 'POST'))
 def login():
+    if request.method =='POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+        user = ''
+
+        if user is None:
+            eroor = 'Incorrect username'
+        #elif not check_password_hash(user['password'], password):
+            #error = 'Incorrect password'
+
+        if error is None:
+            session.clear()
+            #session[]
+            return redirect(url_for('index'))
+
+        flash(error)
+
     return render_template('account/login.html')
+
+
+@login_blueprint.route('/logout/')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))

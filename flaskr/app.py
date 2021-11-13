56a8aea07e43171b652228d0d0c4990cd4f1c1af
BlackSwan-Decoded <56172407+BlackSwan-Decoded@users.noreply.github.com>
2021-11-13 18:20:55 -0500

    added bootstrap to login and register page

--- flaskr/app.py
+++ flaskr/app.py
@@ -7,6 +7,9 @@ def create_app():
     https://flask.palletsprojects.com/en/2.0.x/patterns/appfactories/
     """
     flask = Flask(__name__)
+    flask.config['SECRET_KEY'] = "1a19f9414df3b48c05b67702b5cf7fffdff6964e"
+    flask.config['MONGO_URI'] = "mongodb+srv://nickp:UEuYChybyfDeiRRq@cal0.uud0f.mongodb.net/test"
+
 
     # initialize core blueprints/routes second
     import views

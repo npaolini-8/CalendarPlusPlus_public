6ecfe28847ebe0c5cea9a0929f93e2047e7a4bfa
JehuA <jehujananoria@gmail.com>
2021-11-20 10:19:20 -0500

    basic extendable layout

--- flaskr/views/calendar.py
+++ flaskr/views/calendar.py
@@ -6,5 +6,5 @@ cal_blueprint = Blueprint("calendar", __name__, url_prefix='/calendar')
 
 @cal_blueprint.route('/my_calendar')
 def calendar():
-    return render_template('calendar/calendar.html')
+    return render_template('calendar/month.html')
 

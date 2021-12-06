d7da0808ff0be09a391e06b692dea451b3aea449
gouldt86 <gouldt86@students.rowan.edu>
2021-12-06 12:09:46 -0500

    fixing buttons

---
+++ flaskr/python_helpers/day_functions.py
@@ -0,0 +1,28 @@
+import  calendar as pycal
+
+from datetime import datetime
+from dateutil.relativedelta import relativedelta as rd
+
+
+cal = pycal.Calendar(6)
+current_date = datetime.today()
+day = current_date.day
+month = current_date.month
+year = current_date.year
+
+def get_current_day():
+    return day, month, year
+
+def move(direction):
+    global current_date
+    global day
+    global month
+    global year
+    if direction == 'prev':
+        current_date = current_date - rd(days=1)
+    else:
+        current_date = current_date + rd(days=1)
+    
+    day = current_date.day
+    month = current_date.month
+    year = current_date.year

94de3abf8f3fd05182a204618519d4aef2a74cb2
BlackSwan-Decoded <56172407+BlackSwan-Decoded@users.noreply.github.com>
2021-12-09 18:06:01 -0500

    trying to populate modal with event data if a cell with an event is clicked for updates

--- flaskr/static/javascript/calendar/cells.js
+++ flaskr/static/javascript/calendar/cells.js
@@ -2,7 +2,14 @@ document.querySelectorAll(".cells").forEach(
     day => {
         day.addEventListener("click", event => {
             console.log(event.currentTarget)
-            event.currentTarget.classList.toggle("bg-info")
+            event.currentTarget.classList
+                .toggle("selected")
+            var name = event.currentTarget.cellIndex
+            var date = event.currentTarget.getAttribute("data-date")
+            var time = event.currentTarget.getAttribute("data-time")
+            console.log(name)
+            console.log(date)
+            console.log(time)
         })
     }
 )

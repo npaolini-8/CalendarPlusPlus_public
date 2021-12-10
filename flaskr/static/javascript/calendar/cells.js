519411c53738bf333d4c0ee50c17305ebac1888d
BlackSwan-Decoded <56172407+BlackSwan-Decoded@users.noreply.github.com>
2021-12-10 01:01:10 -0500

    populate modal with parsed data

--- flaskr/static/javascript/calendar/cells.js
+++ flaskr/static/javascript/calendar/cells.js
@@ -7,21 +7,24 @@ document.querySelectorAll(".cells").forEach(
 
             var event_div = event.currentTarget.querySelector(".events")
             if(event_div != null|undefined){
-                event_id = event_div.innerHTML
+                event_id = event_div.innerHTML.trim()
                 event_date = (event_div.id).split("-")
-                event_day = event_date[0].substring(4)
+                event_day = event_date[0].substring(5)
                 event_month = event_date[1]
                 event_year = event_date[2]
-                event_hour = event_date[3]
-            }
-            else{
-                event_id = event.currentTarget.getAttribute("id")
-                event_date = event.currentTarget.getAttribute("id")
-                event_time = event.currentTarget.getAttribute("id")
+                event_hour = Number(event_date[3])%12
+                event_hour = event_hour < 10 ? "0" + event_hour + ":00" : event_hour + ":00"
+                event_date = new Date(event_year + "/" + event_month + "/" + event_day)
+
+
+                document.querySelector(".modal-body #event-name ").value = event_id
+                document.querySelector(".modal-body #event-start-date ").valueAsDate = event_date
+                document.querySelector(".modal-body #event-end-date ").valueAsDate = event_date
+                document.querySelector(".modal-body #event-start-time ").value = event_hour
+                document.querySelector(".modal-body #event-end-time ").value = event_hour
             }
 
-            console.log(event_id)
-            console.log(event_date)
+
         })
     }
 )

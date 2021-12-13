70e6645a62f50f2f1b1452512604db2806251bb1
gouldt86 <gouldt86@students.rowan.edu>
2021-12-12 20:20:10 -0500

    working on cells and event_modal

--- flaskr/static/javascript/calendar/cells.js
+++ flaskr/static/javascript/calendar/cells.js
@@ -1,8 +1,8 @@
-document.querySelectorAll(".cells").forEach(day => {
-        day.addEventListener("click", event => {
-            console.log(event.currentTarget)
-            const cell_id = event.currentTarget.id
-            const event_div = event.currentTarget.querySelector(".events")
+document.querySelectorAll(".cells").forEach(cell => {
+        cell.addEventListener("click", (e) => {
+            console.log(e.currentTarget)
+            const cell_id = e.currentTarget.id
+            const event_div = e.currentTarget.querySelector(".events")
             const current_page = document.location.pathname.split("/")[2]
 
             if(current_page == "month"){
@@ -13,34 +13,57 @@ document.querySelectorAll(".cells").forEach(day => {
             if(event_div != null|undefined) {
                 myModal = new bootstrap.Modal(document.getElementById("event-dialog-modal"))
                 document.getElementById("event-dialog-update").onclick = () => {
-                   event_id = event_div.innerHTML.trim()
-                   event_date = event_div.id.split("-")
-                   event_day = event_date[1]
-                   event_month = event_date[2]
-                   event_year = event_date[3]
+                    the_event = event_div.id.split(",")         
+                    event_id = the_event[0]
+                    start_year = the_event[1]
+                    start_month = the_event[2]
+                    start_day = the_event[3]
+                    start_hour = the_event[4]
+                    start_min = the_event[5]
+                    end_year = the_event[6]
+                    end_month = the_event[7]
+                    end_day = the_event[8]
+                    end_hour = the_event[9]
+                    end_min = the_event[10]
+                    event_loc = the_event[11]
+                    event_desc = the_event[12]//all event values populated
 
-                   if (current_page != "month") {
-                       event_hour = Number(event_date[3]) % 12
-                       event_hour = event_hour < 10 ? "0" + event_hour + ":00" : event_hour + ":00"
+                    start_min = Number(start_min)
+                    start_min = start_min < 10 ? start_min + "0" : String(start_min)
 
-                       document.querySelector(".modal-body #event-start-time ").value = event_hour
-                       document.querySelector(".modal-body #event-end-time ").value = event_hour
-                   }
+                    start_hour = Number(start_hour)%12 
+                    start_time = start_hour < 10 ? "0" + start_hour + ":" + start_min : start_hour + ":" + start_min
 
-                   event_date = new Date(event_year + "/" + event_month + "/" + event_day)
-                   document.querySelector(".modal-body #event-name ").value = event_id
-                   document.querySelector(".modal-body #event-start-date ").valueAsDate = event_date
-                   document.querySelector(".modal-body #event-end-date ").valueAsDate = event_date
+                    end_hour = Number(end_hour)%12
+                    end_time = end_hour < 10 ? "0" + end_hour + ":" + end_min : end_hour + ":" + end_min
 
-                   document.querySelector(".modal-footer #old-title ").value = event_id
-                   document.querySelector(".modal-footer #old-start-time ").value = event_hour
-                   document.querySelector(".modal-footer #old-end-time ").value = event_hour
-                   document.querySelector(".modal-footer #old-start-day ").valueAsDate = event_date
-                   document.querySelector(".modal-footer #old-end-day ").valueAsDate = event_date
 
-                   document.getElementById("event-save").hidden = true
-                   document.getElementById("event-update").hidden = false
-                   document.getElementById("event-delete").hidden = false
+                    document.querySelector(".modal-body #event-start-time ").value = start_time
+                    document.querySelector(".modal-body #event-end-time ").value = end_time
+
+                    start_date = new Date(start_year + "/" + start_month + "/" + start_day)
+                    end_date = new Date(end_year + "/" + end_month + "/" + end_day)
+                    document.querySelector(".modal-body #event-name ").value = event_id
+                    document.querySelector(".modal-body #event-start-date ").valueAsDate = start_date
+                    document.querySelector(".modal-body #event-end-date ").valueAsDate = end_date
+                    document.querySelector(".modal-body #event-description").value = event_desc
+                    document.querySelector(".modal-body #event-location").value = event_loc
+
+                    document.querySelector(".modal-footer #old-title ").value = event_id
+                    document.querySelector(".modal-footer #old-start-time ").value = start_time
+                    document.querySelector(".modal-footer #old-end-time ").value = end_time
+                    document.querySelector(".modal-footer #old-start-date ").valueAsDate = start_date
+                    document.querySelector(".modal-footer #old-end-date ").valueAsDate = end_date
+
+                    document.getElementById("event-save").hidden = true
+                    document.getElementById("event-update").hidden = false
+                    document.getElementById("event-delete").hidden = false
+
+                    document.getElementById("old-title").hidden = true
+                    document.getElementById("old-start-time").hidden = true
+                    document.getElementById("old-end-time").hidden = true
+                    document.getElementById("old-start-date").hidden = true
+                    document.getElementById("old-end-date").hidden = true
                }
 
                document.getElementById("event-dialog-add").onclick = () => {
@@ -73,4 +96,10 @@ function reset(cell_id) {
     document.getElementById("event-save").hidden = false
     document.getElementById("event-update").hidden = true
     document.getElementById("event-delete").hidden = true
+
+    document.getElementById("old-title").hidden = true
+    document.getElementById("old-start-time").hidden = true
+    document.getElementById("old-end-time").hidden = true
+    document.getElementById("old-start-date").hidden = true
+    document.getElementById("old-end-date").hidden = true
 }
